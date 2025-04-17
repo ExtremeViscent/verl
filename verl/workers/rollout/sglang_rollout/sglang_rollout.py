465a2e67e30feb345308233c1ec16e3dbcf1f3f6
ExtremeViscent <limitviscent@gmail.com>
2025-04-17 11:38:39 +0000

    Bugfix

--- verl/workers/rollout/sglang_rollout/sglang_rollout.py
+++ verl/workers/rollout/sglang_rollout/sglang_rollout.py
@@ -454,7 +454,8 @@ class SGLangRollout(BaseRollout):
                     finished_rids.append(rid)
                     if n_finished[oid] == len(cache[oid]) and len(finished_oids) < batch_size:
                         finished_oids.append(oid)
-                        self.group_cache.pop(oid)
+        for oid in finished_oids:
+            cache.pop(oid)
         # Clean up unfinished requests
         if not self.partial_rollout:
             for i, rid in enumerate(rids):
